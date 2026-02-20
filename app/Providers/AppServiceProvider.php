58c5311eb75574021d900cdfb3860ef70b965791
Muhammad Fadhlan Pratama <muhammadfadhlanaja27@gmail.com>
2026-02-20 14:07:27 +0700

    hosting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,40 +1,25 @@
 <?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $tempPath = '/tmp/storage/framework';
+        $viewsPath = $tempPath . '/views';
+
+        if (!is_dir($viewsPath)) {
+            mkdir($viewsPath, 0777, true);
+        }
+
+        config(['view.compiled' => $viewsPath]);
+        config(['session.files' => $tempPath . '/sessions']);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Cek apakah berjalan di Vercel
-        if (config('app.env') === 'production') {
-            // Tentukan folder temporary yang diizinkan Vercel
-            $tempPath = '/tmp/storage/framework';
-            $viewsPath = $tempPath . '/views';
-
-            // Buat struktur folder jika belum ada
-            if (!is_dir($viewsPath)) {
-                mkdir($viewsPath, 0777, true);
-            }
-
-            // Paksa Laravel menggunakan folder /tmp untuk compile view
-            config(['view.compiled' => $viewsPath]);
-
-            // Tambahan: Pastikan folder sessions juga aman (opsional tapi disarankan)
-            config(['session.files' => $tempPath . '/sessions']);
-        }
+        //
     }
 }
